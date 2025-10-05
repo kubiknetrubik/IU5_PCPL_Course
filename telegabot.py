069e4e2e31576371100604fa66b8c49076f1963d
sasku <serkub06@yandex.ru>
2025-10-05 21:20:11 +0300

    (

---
+++ telegabot.py
@@ -0,0 +1,13 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('BOT-TOKEN')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Поздороваться")
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "👋 Привет! Я твой бот-помошник!", reply_markup=markup)
+bot.polling(none_stop=True, interval=0)
